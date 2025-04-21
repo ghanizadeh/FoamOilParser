83093a641ac41ff858d4dcbbee9238d51834651a
ghanizadeh <afshin.ghanizadeh@gmail.com>
2025-04-21 10:42:50 -0600

    Update foam_oil_parser.py

--- foam_oil_parser.py
+++ foam_oil_parser.py
@@ -81,7 +81,7 @@ if uploaded_file:
     search_id = st.text_input("Enter SampleID (case-sensitive):")
 
     if search_id:
-        result_df = df[df["SampleID"] == search_id]
+        result_df = df[df["SampleID"].str.lower() == search_id.lower()]
         if result_df.empty:
             st.warning("No matching SampleID found.")
         else:
