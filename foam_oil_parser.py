5bcdf3557dbae5f9c44ef80610eee61795d10526
ghanizadeh <afshin.ghanizadeh@gmail.com>
2025-04-21 15:47:22 -0600

    Update foam_oil_parser.py

--- foam_oil_parser.py
+++ foam_oil_parser.py
@@ -25,7 +25,7 @@ if utl.check_password():
         df["Dilution"] = df["Dilution"].str.replace(r"\b[Dd]ilution\b", "", regex=True).str.strip()
         df["Dilution"] = df["Dilution"].str.replace(r"-{2,}", " - ", regex=True).str.strip()
         df["Dilution"] = df["Dilution"].fillna("00x")
-        #df["Date"] = df["Date"].fillna("No Date")
+        df["Date"] = df["Date"].fillna("No Date")
 
         parsed_df = df.copy()
         parsed_df["Time (min)"] = parsed_df["Time (min)"].apply(utl.convert_to_minutes)
